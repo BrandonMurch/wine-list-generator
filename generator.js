982ac09e8c4aa23f458637854cd27c04bc845de3
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2021-01-31 05:22:50 +0200

    Refactor to follow AirBnB guide
    Signed-off-by: BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>

--- generator.js
+++ generator.js
@@ -1,7 +1,8 @@
 function createWineList() {
+  // Global Configurations
   const HEADERS = SpreadsheetApp.getActiveSpreadsheet().getSheetValues(1, 1, 1, -1)[0];
-  const TEMPLATE_ID = "12yfe6AowMOBML7pkagvPJT_5M-APyuiSzcCSJo3I_80";
-  const FOLDER_ID = "1pTbUMcLlU2q-ZaLLouGSGRiYgdHjlN4U";
+  const TEMPLATE_ID = '12yfe6AowMOBML7pkagvPJT_5M-APyuiSzcCSJo3I_80';
+  const FOLDER_ID = '1pTbUMcLlU2q-ZaLLouGSGRiYgdHjlN4U';
   const MAX_LINES = 54;
   const CUVEE_SIZE = 9;
   const COUNTRY_LINES = 21 / CUVEE_SIZE;
@@ -16,39 +17,75 @@ function createWineList() {
   let readCounter = 0;
   let writeCounter = 0;
   let percentage = 0;
-
-  function getHeaderIndex(headerString) {
-    return HEADERS.indexOf(headerString);
-  }
+  let getAppendFunction;
 
   function toast(message) {
-      SpreadsheetApp.getActiveSpreadsheet().toast(message);
+    SpreadsheetApp.getActiveSpreadsheet().toast(message);
   }
 
   function alert(message) {
     SpreadsheetApp.getUi().alert(message);
   }
 
+  function loadOutOfStockWines() {
+    toast('Loading out of stock wines...');
+    function getTrueInventory(row, headers) {
+      return parseInt(row[headers.indexOf('Cellar')], 10) + parseInt(row[headers.indexOf('Online Store')], 10);
+    }
+
+    function getOutOfStockList(sheet, headers) {
+      const outOfStockList = [];
+      sheet.forEach((row) => {
+        if (getTrueInventory(row, headers) <= 0) {
+          outOfStockList.push(row[headers.indexOf('Title')].toString());
+        }
+      });
+      return outOfStockList;
+    }
+
+    const sheetUrl = SpreadsheetApp.getUi()
+      .prompt('Please enter the URL of the inventory sheet.')
+      .getResponseText();
+    if (!sheetUrl) {
+      throw new Error('URL was not entered.');
+    }
+
+    let outOfStockSheet;
+
+    try {
+      outOfStockSheet = SpreadsheetApp.openByUrl(sheetUrl);
+    } catch (error) {
+      throw new Error('URL not found');
+    }
+    const sheetHeaders = outOfStockSheet.getSheetValues(1, 1, 1, -1)[0];
+    const sheetValues = outOfStockSheet.getSheetValues(2, 1, -1, -1);
+    return getOutOfStockList(sheetValues, sheetHeaders);
+  }
+
+  function getHeaderIndex(headerString) {
+    return HEADERS.indexOf(headerString);
+  }
+
   function getFormattedName(wine) {
-    const nameCell = wine[getHeaderIndex("Name")];
-    const vintage = nameCell.match(/\d{4}|NV/) || "";
+    const nameCell = wine[getHeaderIndex('Name')];
+    const vintage = nameCell.match(/\d{4}|NV/) || '';
     // RegExp: matches all words between double quotations
     let name = nameCell.match(/(?<=").+(?=")/);
     // RegExp: matches all words between parenthesis
     const size = nameCell.match(/(?<=\().+(?=\))/);
     if (size) {
-      name += " (" + size + ")";
+      name += ` (${size})`;
     }
 
-    return vintage + " " + name;
+    return `${vintage} ${name}`;
   }
 
   function getFormattedGrapes(wine) {
-    const grapes = wine[getHeaderIndex("Grapes")];
-    if (grapes.includes("/")) {
+    const grapes = wine[getHeaderIndex('Grapes')];
+    if (grapes.includes('/')) {
       // RegExp: matches "/word" or "/word word" which is followed
       // either by a "," or the end of line
-      return grapes.replace(/\/[\w ]+(?=(,|$))/g, "");
+      return grapes.replace(/\/[\w ]+(?=(,|$))/g, '');
     }
     return grapes;
   }
@@ -57,73 +94,74 @@ function createWineList() {
     return {
       name: getFormattedName(wine),
       grapes: getFormattedGrapes(wine),
-      price: wine[getHeaderIndex("Restaurant Price")],
-      macerated: wine[getHeaderIndex("Type")] == "orange",
+      price: wine[getHeaderIndex('Restaurant Price')],
+      macerated: wine[getHeaderIndex('Type')] === 'orange',
     };
   }
 
   function fixCategoryNames(category) {
-    if (category == "white" || category == "orange") {
-      return "white & macerated";
-    } else if (category == "redwine") {
-      return "red";
+    if (category === 'white' || category === 'orange') {
+      return 'white & macerated';
+    } if (category === 'redwine') {
+      return 'red';
     }
     return category;
   }
 
   function createStackForTrie(wine) {
     return [
-      fixCategoryNames(wine[getHeaderIndex("Type")]),
-      wine[getHeaderIndex("Country")],
-      wine[getHeaderIndex("Region")].toLowerCase(),
-      wine[getHeaderIndex("Producer")],
+      fixCategoryNames(wine[getHeaderIndex('Type')]),
+      wine[getHeaderIndex('Country')],
+      wine[getHeaderIndex('Region')].toLowerCase(),
+      wine[getHeaderIndex('Producer')],
     ];
   }
 
-  const insertIntoTrie = function recursivelyCheckTrieHasBeenInitialisedAndInsertCuvee(map, stack, cuvee) {
-    let nodeName = stack.shift();
+  const insertIntoTrie = function recursivelyCheckTrieHasBeenInitialisedAndInsertCuvee(
+    map, stack, cuvee,
+  ) {
+    const nodeName = stack.shift();
     if (stack.length === 0) {
       if (map[nodeName] === undefined) {
         map[nodeName] = [];
       }
       map[nodeName].push(cuvee);
     } else {
-      if (map[nodeName] == undefined) {
+      if (map[nodeName] === undefined) {
         map[nodeName] = {};
       }
       insertIntoTrie(map[nodeName], stack, cuvee);
     }
   };
 
-  function shouldWineGoOnList(wine) {
-    const typeIndex = getHeaderIndex("Type");
-    if (wine[typeIndex] == "not-wine" 
-    || wine[typeIndex] == "fortified"
-    || wine[getHeaderIndex("Hide From Wine List")]
+  function shouldWineGoOnList(wine, outOfStockWines) {
+    const typeIndex = getHeaderIndex('Type');
+    if (wine[typeIndex] === 'not-wine'
+    || wine[typeIndex] === 'fortified'
+    || wine[getHeaderIndex('Hide From Wine List')]
     || wine[typeIndex].length === 0) {
       return false;
     }
 
     // The website still stores names in between single quotations, when this changes
     // to double quotations, this line can be removed.
-    const modifiedName = wine[getHeaderIndex("Name")].split("\"").join("'");
+    const modifiedName = wine[getHeaderIndex('Name')].split('"').join("'");
     return !outOfStockWines.includes(modifiedName);
   }
 
   function loadWineIntoMap(wine, map) {
-    readCounter++;
+    readCounter += 1;
     const cuvee = createCuvee(wine);
     const stack = createStackForTrie(wine);
     insertIntoTrie(map, stack, cuvee);
-
   }
 
-  function loadWinesIntoHashMap() {
-    toast("Reading wines from spreadsheet...")
+  function loadWinesIntoHashMap(outOfStockWines) {
+    toast('Reading wines from spreadsheet...');
     const wineMap = {};
     const wineSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetValues(3, 1, -1, -1);
     for (let i = 0; i < wineSheet.length; i++) {
-      if (shouldWineGoOnList(wineSheet[i])) {
+      if (shouldWineGoOnList(wineSheet[i], outOfStockWines)) {
         loadWineIntoMap(wineSheet[i], wineMap);
       }
     }
@@ -133,8 +171,8 @@ function createWineList() {
   function appendOnNewPageIfNeeded(part, writing) {
     const { current } = writing;
     if (current[part]) {
-      let textToInsert = current[part];
-      if (part != "region") {
+      const textToInsert = current[part];
+      if (part !== 'region') {
         const append = getAppendFunction(part);
         append(textToInsert, writing);
       }
@@ -142,7 +180,7 @@ function createWineList() {
   }
 
   function getStackOrder() {
-    return ["category", "country", "region"];
+    return ['category', 'country', 'region'];
   }
 
   function appendPageBreak(writing) {
@@ -168,11 +206,19 @@ function createWineList() {
   function appendCountryImage(country, writing) {
     const { document } = writing;
     const image = writing.images[country];
-    const paragraph = document.getBody().appendParagraph("");
+    const paragraph = document.getBody().appendParagraph('');
     paragraph.addPositionedImage(image);
     correctImagePosition(paragraph.getPositionedImages()[0]);
     setFontSizeOfParagraph(18, paragraph);
     pageLineCounter += COUNTRY_LINES;
+  }
+
+  function appendLineToDocument(line, document) {
+    if (line.getType() === 'PARAGRAPH') {
+      document.appendParagraph(line);
+    } else if (line.getType() === 'TABLE') {
+      document.appendTable(line);
+    }
   }
 
   function appendCategory(category, { templates, document }) {
@@ -181,7 +227,7 @@ function createWineList() {
   }
 
   function formatPrice(price) {
-    return parseInt(price);
+    return parseInt(price, 10);
   }
 
   function getProgressPercentage() {
@@ -189,7 +235,7 @@ function createWineList() {
   }
 
   function updateProgress() {
-    writeCounter++;
+    writeCounter += 1;
     if (getProgressPercentage() >= percentage + 10) {
       SpreadsheetApp.getActiveSpreadsheet().toast(`Writing to template: ${Math.round(getProgressPercentage())}% completed`);
       percentage = getProgressPercentage();
@@ -201,7 +247,7 @@ function createWineList() {
     const y = b.name.toLowerCase();
     if (x < y) {
       return -1;
-    } else if (y < x) {
+    } if (y < x) {
       return 1;
     }
     return 0;
@@ -238,11 +284,11 @@ function createWineList() {
     pageLineCounter += REGION_LINES;
 
     producerNames.forEach((producerName, index) => {
-      if (index != 0 && willProducerExtendToNextPage(producers[producerName])) {
+      if (index !== 0 && willProducerExtendToNextPage(producers[producerName])) {
         appendLineToDocument(table, document);
         appendPageBreak(writing);
         table = templates.table();
-        const continuedRegion = templates.region(region + " cont'd");
+        const continuedRegion = templates.region(`${region} cont'd`);
         pageLineCounter += REGION_LINES;
         table.appendTableRow(continuedRegion);
       }
@@ -253,29 +299,28 @@ function createWineList() {
     setFontSizeOfParagraph(11, getLastChild(document));
   }
 
-  function getAppendFunction(type) {
+  getAppendFunction = (type) => {
     const appendFunctions = {
       region: appendRegion,
       country: appendCountryImage,
       category: appendCategory,
     };
     return appendFunctions[type];
-  }
+  };
 
   function hasEndOfPageBeenReached(dataType) {
     return pageLineCounter > (MAX_LINES - LINES_NEEDED[dataType]);
   }
 
   function getCountryOrder() {
-    return ["France", "Italy", "Austria", "Germany", "Australia", "South Africa"];
+    return ['France', 'Italy', 'Austria', 'Germany', 'Australia', 'South Africa'];
   }
 
   function getKeys(type, data) {
-    if (type == "country") {
+    if (type === 'country') {
       return getCountryOrder();
-    } else {
-      return Object.keys(data).sort();
     }
+    return Object.keys(data).sort();
   }
 
   function appendNext(data, dataTypeStack, writing) {
@@ -296,7 +341,7 @@ function createWineList() {
         appendNext(data[key], dataTypeStack, writing);
       }
       current[dataType] = undefined;
-      if (dataType === "category") {
+      if (dataType === 'category') {
         document.appendPageBreak();
         pageLineCounter = 0;
       }
@@ -307,33 +352,29 @@ function createWineList() {
   function loadTemplate() {
     const templateBody = DocumentApp.openById(TEMPLATE_ID).getBody().copy();
     const table = templateBody.getChild(2).asTable();
-    const template = {
+    const templates = {
       category: (textToInsert) => {
-        const template = templateBody.getChild(0).copy().asParagraph().replaceText("{{category}}", textToInsert);
+        const template = templateBody.getChild(0).copy().asParagraph().replaceText('{{category}}', textToInsert);
 
-        template.asParagraph().replaceText("{{category_maceration}}", textToInsert == "white & macerated" ? " ▴" : "");
+        template.asParagraph().replaceText('{{category_maceration}}', textToInsert === 'white & macerated' ? ' ▴' : '');
 
         return template;
       },
-      region: (textToInsert) => {
-        return table.getRow(0).copy().replaceText("{{region}}", textToInsert);
-      },
-      producer: (textToInsert) => {
-        return table.getRow(1).copy().replaceText("{{producer}}", textToInsert);
-      },
-      cuvee: ({ name, grapes, price, macerated }) => {
+      region: (textToInsert) => table.getRow(0).copy().replaceText('{{region}}', textToInsert),
+      producer: (textToInsert) => table.getRow(1).copy().replaceText('{{producer}}', textToInsert),
+      cuvee: ({
+        name, grapes, price, macerated,
+      }) => {
         const template = table.getRow(2).copy();
-        template.replaceText("{{cuvee}}", name);
-        template.replaceText("{{grapes}}", grapes);
-        template.replaceText("{{price}}", formatPrice(price).toString());
-        template.replaceText("{{cuvee_maceration}}", macerated ? " ▴" : "");
+        template.replaceText('{{cuvee}}', name);
+        template.replaceText('{{grapes}}', grapes);
+        template.replaceText('{{price}}', formatPrice(price).toString());
+        template.replaceText('{{cuvee_maceration}}', macerated ? ' ▴' : '');
         return template;
       },
-      table: () => {
-        return table.copy().clear();
-      },
+      table: () => table.copy().clear(),
     };
-    return template;
+    return templates;
   }
 
   function setTopAndBottomMargins(document) {
@@ -345,7 +386,7 @@ function createWineList() {
 
   function createNewWineListFile() {
     const folder = DriveApp.getFolderById(FOLDER_ID);
-    const wineList = DocumentApp.create("Wine List " + new Date().toDateString());
+    const wineList = DocumentApp.create(`Wine List ${new Date().toDateString()}`);
     folder.addFile(DriveApp.getFileById(wineList.getId()));
     setTopAndBottomMargins(wineList);
     return wineList;
@@ -355,7 +396,7 @@ function createWineList() {
     const countries = getCountryOrder();
     const images = {};
     countries.forEach((country) => {
-      images[country] = DriveApp.getFilesByName(country + ".png")
+      images[country] = DriveApp.getFilesByName(`${country}.png`)
         .next()
         .getBlob();
     });
@@ -375,54 +416,11 @@ function createWineList() {
     writing.document.saveAndClose();
   }
 
-  function appendLineToDocument(line, document) {
-    if (line.getType() == "PARAGRAPH") {
-      document.appendParagraph(line);
-    } else if (line.getType() == "TABLE") {
-      document.appendTable(line);
-    }
-  }
-
-  alert("Please stay on this sheet until the script has completed.")
+  alert('Please stay on this sheet until the script has completed.');
   const outOfStockWines = loadOutOfStockWines();
-  const wines = loadWinesIntoHashMap();
+  const wines = loadWinesIntoHashMap(outOfStockWines);
   writeWinesToTemplate(wines);
-  toast("100% completed");
+  toast('100% completed');
 }
 
-
-function loadOutOfStockWines() {
-  toast("Loading out of stock wines...")
-  function getTrueInventory(row) {
-    return parseInt(row[sheetHeaders.indexOf("Cellar")]) + parseInt(row[sheetHeaders.indexOf("Online Store")]);
-  }
-
-  function getOutOfStockList(sheet, headers) {
-    const outOfStockList = [];
-    sheetValues.forEach((row) => {
-      if (getTrueInventory(row) <= 0) {
-        outOfStockList.push(row[sheetHeaders.indexOf("Title")].toString());
-      } 
-    });
-  return outOfStockList;
-  } 
-
-  const sheetUrl = SpreadsheetApp.getUi()
-    .prompt("Please enter the URL of the inventory sheet.")
-    .getResponseText();
-  if (!sheetUrl) {
-    throw new Error("URL was not entered.");
-  }
-
-  let outOfStockSheet;
-
-  try {
-   outOfStockSheet = SpreadsheetApp.openByUrl(sheetUrl);
-  } catch (error) {
-    throw new Error("URL not found")
-  }
-  const sheetHeaders = outOfStockSheet.getSheetValues(1, 1, 1, -1)[0];
-  const sheetValues = outOfStockSheet.getSheetValues(2, 1, -1, -1);
-  return getOutOfStockList(sheetValues, sheetHeaders);
-
-}
+export default createWineList;
